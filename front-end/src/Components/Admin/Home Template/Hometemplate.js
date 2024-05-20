239f6d426ff249f0dcbd10f1bd81898eb0a535b4
vigneshm138 <vigneshmb138@gmail.com>
2024-05-20 19:25:10 +0530

    addd

--- front-end/src/Components/Admin/Home Template/Hometemplate.js
+++ front-end/src/Components/Admin/Home Template/Hometemplate.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react'
 import '../../Commonformstyle.css'
+import axios from 'axios'
 
 const Hometemplate = () => {
 
 
     const [hrs, sethrs] = useState()
-    const [placeType, setplaceType] = useState("")
+    const [placeType, setplaceType] = useState("mall")
     const [placeName, setplaceName] = useState()
     const [location, setlocation] = useState()
     const [area, setarea] = useState()
@@ -15,8 +16,9 @@ const Hometemplate = () => {
     const [wedo, setwedo] = useState()
 
 
-    const handleSubmit=(e)=>{
+    const handleSubmit = (e) => {
         e.preventDefault()
+        axios.post("http://localhost:3080/data", { hrs, placeType, placeName, location, area, img, keyPoints: highlights, heading, whatwedo: wedo })
         console.log(hrs);
         console.log(placeType);
         console.log(placeName);
@@ -24,21 +26,21 @@ const Hometemplate = () => {
         console.log(area);
         console.log(img);
         console.log(highlights);
+        console.log(heading);
         console.log(wedo);
     }
     return (
         <div>
             <div className='commonFormStyle'>
-                <form onSubmit={(e)=>handleSubmit(e)}>
+                <form onSubmit={(e) => handleSubmit(e)}>
                     <h1>home Template</h1>
                     <div>
                         <label>HRS</label>
-                        <input type='number' value={hrs} onChange={(e) => sethrs(e.target.value)} required/>
+                        <input type='number' value={hrs} onChange={(e) => sethrs(e.target.value)} required />
                     </div>
                     <div>
                         <label>place name</label>
                         <select value={placeType} onChange={(e) => setplaceType(e.target.value)} required>
-                            <option value={"select"}>select u want</option>
                             <option value={"mall"}>mall</option>
                             <option value={"cinema"}>cinema</option>
                             <option value={"lake"}>lake</option>
@@ -46,7 +48,7 @@ const Hometemplate = () => {
                     </div>
                     <div>
                         <label>name</label>
-                        <input type='text' placeholder=' prozone mall' value={placeName} onChange={(e) => setplaceName(e.target.value)} required/>
+                        <input type='text' placeholder=' prozone mall' value={placeName} onChange={(e) => setplaceName(e.target.value)} required />
                     </div>
                     <div>
                         <label>location</label>
@@ -54,7 +56,7 @@ const Hometemplate = () => {
                     </div>
                     <div>
                         <label>start area</label>
-                        <input type='text' value={area} onChange={(e) => setarea(e.target.value)} required/>
+                        <input type='text' value={area} onChange={(e) => setarea(e.target.value)} required />
                     </div>
                     <div>
                         <label>img</label>
@@ -62,7 +64,7 @@ const Hometemplate = () => {
                     </div>
                     <div>
                         <label>place highlights</label>
-                        <textarea placeholder='only give simple 3 key points' value={highlights} onChange={(e) => sethighlights(e.target.value)} required/>
+                        <textarea placeholder='only give simple 3 key points' value={highlights} onChange={(e) => sethighlights(e.target.value.split(','))} required />
                     </div>
                     <div>
                         <label>heading</label>
@@ -70,7 +72,7 @@ const Hometemplate = () => {
                     </div>
                     <div>
                         <label>what u do</label>
-                        <textarea placeholder='only give simple 3 key points' value={wedo} onChange={(e) => setwedo(e.target.value)} required/>
+                        <textarea placeholder='only give simple 3 key points' value={wedo} onChange={(e) => setwedo(e.target.value.split(','))} required />
                     </div>
                     <button type='submit'>make home Template</button>
                 </form>
